e7e01107108aed712847254970f3761fff203e02
Philipp Diercks <55233917+pdiercks@users.noreply.github.com>
2023-08-14 16:18:19 +0200

    [124-Unnecessary-Dependencies] refactor environment.yml and pyproject.toml (#126)
    * refactor environment.yml and pyproject.toml
    
    * add composite action to install extra deps for gmsh
    
    * [workflows][push_tests] install extra deps
    
    * [install extra deps] add libglu1-mesa
    
    * [boundary.show_marked] exclude function from coverage; remove tests depending on matplotlib
    
    * [boundary] rename show_marked
    
    * [pyproject] add matplotlib to optitonal deps
    
    * [boundary] matplotlib is required to show marked dofs
    
    * isort

--- src/fenicsxconcrete/boundary_conditions/__init__.py
+++ src/fenicsxconcrete/boundary_conditions/__init__.py
@@ -1,2 +1,2 @@
 from .bcs import BoundaryConditions, get_boundary_dofs
-from .boundary import create_facet_tags, line_at, plane_at, point_at, show_marked, to_floats, within_range
+from .boundary import _show_marked, create_facet_tags, line_at, plane_at, point_at, to_floats, within_range
